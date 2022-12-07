9be3294ce8d7a92ba08f9d5a1d390e689f7be077
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-12-07 11:00:21 +0100

    Add sign up layout + add lost password layout + add action bar

--- app/src/main/java/fr/android/steam/MainActivity.kt
+++ app/src/main/java/fr/android/steam/MainActivity.kt
@@ -1,20 +1,38 @@
 package fr.android.steam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lost_password)
 
-        supportActionBar?.hide(); // Hide action bar on top
+        // Show action bar
+        supportActionBar?.setCustomView(R.layout.action_bar)
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+
+        /* Test Image button in navbar
+        findViewById<ImageButton>(R.id.navbar_button_like).setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<ImageButton>(R.id.navbar_button_wishlist).setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+        */
+
+        // Hide action bar on top
+        // supportActionBar?.hide();
 
         // Set error style on input
         /*
-        val inputPassword = findViewById<EditText>(R.id.input_password);
-        inputPassword.setBackgroundResource(R.drawable.error_input);
-        inputPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.warning, 0);
-        */
+            val inputPassword = findViewById<EditText>(R.id.sign_in_input_email);
+            inputPassword.setBackgroundResource(R.drawable.error_input);
+            inputPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.warning, 0);
+         */
+
     }
 }
